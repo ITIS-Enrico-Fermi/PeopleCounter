b5d1473697d1bd521fd45e2050c40b0b0745fa92
manu515 <emanuele.reggiani@fermi.mo.it>
2021-04-20 11:49:40 +0200

    adding function load_yaml

--- config/calibration.py
+++ config/calibration.py
@@ -4,5 +4,10 @@ import logging
 import os
 import numpy as np
 from typing import Tuple, List
+import yaml
 
 
+def load_yaml(filename: str) -> dict:
+	with open(filename) as file:
+		config = yaml.load(file, Loader=yaml.FullLoader)
+	return config 
