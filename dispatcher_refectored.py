d0c14e7e354b7e2cbb7c9bab738b684be3581409
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-09 19:07:05 +0200

    add heading comment

--- dispatcher_refectored.py
+++ dispatcher_refectored.py
@@ -1,4 +1,9 @@
+"""
+dispatcher.py
 
+Dispatcher acts as the central component of the architecture, serving frames to the tracker or the detector according to the provided algorithm.
+"""
+__author__ = "Francesco Mecatti & the PeopleCounter team"
 
 import cv2 as cv
 import argparse
