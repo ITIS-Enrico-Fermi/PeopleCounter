fa662d90fec2ed13b9b7fc1b27914dd737a84046
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-09 19:34:23 +0200

    add heading comment

--- cvlib/__init__.py
+++ cvlib/__init__.py
@@ -1,3 +1,11 @@
+"""
+__init__.py
+
+cvlib is a convenience module containing frequently used objects for frames display and general purposed CV operations
+"""
+
+__author__ = "Francesco Mecatti & the PeopleCounter team"
+
 from .display import *
 from .utils import *
 
