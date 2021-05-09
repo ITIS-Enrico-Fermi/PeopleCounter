e7e943d4a297229a47cd68d96fe8cbc5d5faf39d
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-09 19:15:40 +0200

    minor change

--- dispatcher_refectored.py
+++ dispatcher_refectored.py
@@ -3,6 +3,7 @@ dispatcher.py
 
 Dispatcher acts as the central component of the architecture, serving frames to the tracker or the detector according to the provided algorithm.
 """
+
 __author__ = "Francesco Mecatti & the PeopleCounter team"
 
 import cv2 as cv
