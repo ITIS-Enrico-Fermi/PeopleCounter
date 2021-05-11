7c24900c1dab19fe078c400cd3069f148b167b24
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-11 12:30:15 +0200

    refactoring code

--- dispatcher_refectored.py
+++ dispatcher_refectored.py
@@ -37,8 +37,10 @@ class Dispatcher:
         :param str video_source: video source. If video_source is a string, it's supposed to be the relative path to a file, else video_source is converted to an integer and the video stream is treated like a cam
         """
         self.models_cascade: List[cv.CascadeClassifier] = list()
-        self.video_source: str = video_source  # video_source == None if the classifier will be used on an image
-        self.image: str = image  # image == None if the classifier will be used on the video source
+        self.source = None  # Either a number (webcam) or a string (video file)
+        self.frame: np.ndarray = None
+        # self.video_source: str = video_source  # video_source == None if the classifier will be used on an image
+        # self.image: str = image  # image == None if the classifier will be used on the video source
         self.start_time_int: int = None  # start_time will fill this attribute for the first time
         self.times: np.array = None  # start will fill this attribute
         self.times_index: int = 0  # Index to keep track of times array filling
@@ -123,3 +125,33 @@ class Dispatcher:
         def inner(self):
             self.dispatching_algo = f
         return f
+    
+
+    def __start_time(self) -> None:
+        """
+        Get current time and save it into self.start_time. Used to compute the elapsed time afterwards
+        """
+        self.start_time_int = time.time()
+
+    def __end_time(self) -> None:
+        """
+        Compute elapsed time (between start time and current time) and save it into self.times, in order to figure out what's the average time needed to classify one frame
+        """
+        logging.info(f"time for 1 frame classification {time.time() - self.start_time_int}")
+        if not str.isnumeric(self.video_source):  # If the video source is not a cam
+            self.times[self.times_index] = time.time() - self.start_time_int
+            self.times_index += 1
+
+    def preprocess(self) -> None:
+        """
+        Shared method for frame preprocessing. Frames are preprocessed only once, and then tested against several models, in order to decrease CPU laod and increase recognition speed.
+        Final frame (preprocessed) is set as a context variable
+        """
+        if self.is_first_frame:
+            self.display.set_orientation(self.frame)
+            self.is_first_frame = False
+        self.frame_light = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
+        self.frame_light = cv.resize(self.frame_light, dsize = self.display.size, interpolation = cv.INTER_AREA)
+        # downscaled_frame_gray_equalized: np.ndarray = cv.equalizeHist(downscaled_frame_gray)
+    
+    def detect(self)
