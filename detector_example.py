46f21ea3e6f0ac4b6dd5b7c96f1a11b2b96c0773
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-15 23:59:30 +0200

    prettifying code

--- detector_example.py
+++ detector_example.py
@@ -12,20 +12,26 @@ import os
 from sys import argv
 
 class FrontalFaceDetector(Detector):
-    def config(self):
-        filename = self._model
-        c = cv.CascadeClassifier()
-        c.load(filename)
-        self._detector = c
+	def config(self):
+		filename = self._model
+		detector = cv.CascadeClassifier()
+		detector.load(filename)
+		self._detector = detector
 
-    def detect(self, frame):
-        self._regions = self._detector.detectMultiScale(frame)
-        return True
+	def detect(self, frame):
+		try:
+			self._regions = \
+				self._detector.detectMultiScale(frame)
+		except:
+			return False		
+		return True
 
 if __name__ == "__main__":
-    if len(argv) == 2:  # The second parameter is the filename of the image to be tested against detectors
-        d = FrontalFaceDetector.create() \
-                               .set_model('OpencvModels/haarcascade_frontalface_default.xml') \
-                               .run_config()
-        d.detect(cv.imread(argv[1]))
-        print(d.get_regions())
+	if len(argv) == 2:  # The second parameter is the filename of the image to be tested against detectors
+		d = (FrontalFaceDetector
+			.create()
+			.set_model('OpencvModels/haarcascade_frontalface_default.xml')
+			.run_config())
+
+		d.detect(cv.imread(argv[1]))
+		print(d.get_regions())
