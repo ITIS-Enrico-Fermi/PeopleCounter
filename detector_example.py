dbbe14c7349e6d40ec27461e892b0d85210e6755
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-15 07:34:23 +0200

    add Detector example

---
+++ detector_example.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+from detector import Detector
+import os
+from sys import argv
+
+d = None
+
+class FrontalFaceDetector(Detector):
+    def config(self):
+        filename = self._model
+        c = cv.CascadeClassifier()
+        c.load(filename)
+        self._detector = c
+
+    def detect(self, frame):
+        self._regions = self._detector.detectMultiScale(frame)
+        return True
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        d = FrontalFaceDetector.create() \
+                               .set_model('OpencvModels/haarcascade_frontalface_default.xml') \
+                               .run_config()
+        d.detect(cv.imread(argv[1]))
+        print(d.get_regions())
