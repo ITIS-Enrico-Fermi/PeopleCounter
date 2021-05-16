174f72da25b72e620cb52fd9b1b8fa63f864179f
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-16 07:54:37 +0200

    move context_error to common

--- detector.py
+++ detector.py
@@ -12,24 +12,7 @@ import numpy as np
 import time
 from cvlib import *
 from abc import ABC, ABCMeta, abstractmethod
-
-def context_error(f):
-    """
-    Decorator for error checking/handling.
-    This way methods don't throw error messages,
-    everything stays inside the context
-    """
-    def inner(*args, **kwargs):
-        self = args[0]
-        # Chain of waterfall methods is broken if error
-        try:
-            f(*args, **kwargs)
-        except Exception as e:
-            self._is_error = True
-            self._error = e
-        finally:
-            return self
-    return inner
+from common import context_error
 
 class Detector():
     """
