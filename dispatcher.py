e3fb4d8341e5a84b919e28ea96e614577cda39b3
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2021-05-16 08:01:06 +0200

    changed indentation

--- dispatcher.py
+++ dispatcher.py
@@ -26,9 +26,9 @@ class Dispatcher:
         """
         self.models_cascade: List[cv.CascadeClassifier] = list()
         for model in models:
-            model_cascade: cv.CascadeClassifier = cv.CascadeClassifier()
-            model_cascade.load(cv.samples.findFile(model))
-            self.models_cascade.append(model_cascade)
+					model_cascade: cv.CascadeClassifier = cv.CascadeClassifier()
+					model_cascade.load(cv.samples.findFile(model))
+					self.models_cascade.append(model_cascade)
         self.video_source: str = video_source  # video_source == None if the classifier will be used on an image
         self.image: str = image  # image == None if the classifier will be used on the video source
         self.start_time_int: int = None  # start_time will fill this attribute for the first time
@@ -56,8 +56,8 @@ class Dispatcher:
         """
         logging.info(f"time for 1 frame classification {time.time() - self.start_time_int}")
         if not str.isnumeric(self.video_source):  # If the video source is not a cam
-            self.times[self.times_index] = time.time() - self.start_time_int
-            self.times_index += 1
+					self.times[self.times_index] = time.time() - self.start_time_int
+					self.times_index += 1
     
     def preprocess(self, frame: np.ndarray):
         """
@@ -67,8 +67,8 @@ class Dispatcher:
         """
         frame_gray: np.ndarray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         if self.is_first_frame:
-            self.display.set_orientation(frame)
-            self.is_first_frame = False
+					self.display.set_orientation(frame)
+					self.is_first_frame = False
         downscaled_frame_gray: np.ndarray = cv.resize(frame_gray, dsize = self.display.size, interpolation = cv.INTER_AREA)
         # downscaled_frame_gray_equalized: np.ndarray = cv.equalizeHist(downscaled_frame_gray)
         return downscaled_frame_gray
@@ -86,27 +86,27 @@ class Dispatcher:
         shape: Shape = Shape.RECTANGLE  # Default shape
         self.__start_time()
         for model_cascade, color in zip(self.models_cascade, self.colors):
-            processed_frame = self.preprocess(frame)
-            obj_list = list()
-            if len(set(model_cascade.getOriginalWindowSize())) == 1:  # Face
-                shape = Shape.ELLIPSE
-                obj_list = model_cascade.detectMultiScale(processed_frame, scaleFactor = 1.2, minSize = config_boundaries['face']['min'], maxSize = config_boundaries['face']['max'])
-            else:
-                shape = Shape.RECTANGLE
-                obj_list = model_cascade.detectMultiScale(processed_frame, scaleFactor = 1.2, minSize = config_boundaries['body']['min'], maxSize = config_boundaries['body']['max'])
+					processed_frame = self.preprocess(frame)
+					obj_list = list()
+					if len(set(model_cascade.getOriginalWindowSize())) == 1:  # Face
+						shape = Shape.ELLIPSE
+						obj_list = model_cascade.detectMultiScale(processed_frame, scaleFactor = 1.2, minSize = config_boundaries['face']['min'], maxSize = config_boundaries['face']['max'])
+					else:
+						shape = Shape.RECTANGLE
+						obj_list = model_cascade.detectMultiScale(processed_frame, scaleFactor = 1.2, minSize = config_boundaries['body']['min'], maxSize = config_boundaries['body']['max'])
 
-            scale_factor_x: float = frame.shape[1] / self.display.size[0]  # both shape[1] and size[0] refer to the x (width)
-            scale_factor_y: float = frame.shape[0] / self.display.size[1]  # both shape[0] and size[1] refer to the y (height)
-            for (x, y, w, h) in obj_list:
-                processed_frame_regions_list.append(Region(x, y, w, h, color, shape))
-                original_frame_regions_list.append(Region(x*scale_factor_x, y*scale_factor_y, w*scale_factor_x, h*scale_factor_y, color, shape))
+					scale_factor_x: float = frame.shape[1] / self.display.size[0]  # both shape[1] and size[0] refer to the x (width)
+					scale_factor_y: float = frame.shape[0] / self.display.size[1]  # both shape[0] and size[1] refer to the y (height)
+					for (x, y, w, h) in obj_list:
+						processed_frame_regions_list.append(Region(x, y, w, h, color, shape))
+						original_frame_regions_list.append(Region(x*scale_factor_x, y*scale_factor_y, w*scale_factor_x, h*scale_factor_y, color, shape))
         self.__end_time()
         # if processed_frame_preview:
         #     self.display(processed_frame, processed_frame_regions_list, 'Processed frame preview')
         if not processed_frame_preview:
-            return original_frame_regions_list
+					return original_frame_regions_list
         else:
-            return original_frame_regions_list, processed_frame, processed_frame_regions_list
+          return original_frame_regions_list, processed_frame, processed_frame_regions_list
 
     def detect_and_display(self, frame: np.ndarray, processed_frame_preview: bool) -> None:
         """
@@ -118,63 +118,63 @@ class Dispatcher:
         self.display.show(frame, regions, 'Face detection with HCC', 0.4, frame_processed = frame_p, regions_processed = frame_reg_p)
     
     def track(self, frame: np.ndarray, init_regions: List[Region] = None) -> Tuple[int, int, int, int]:
-        if init_regions is not None:
-            if init_regions:
-                self.tracker = cv.TrackerKCF_create()
-                self.tracker.init(frame, init_regions[0].to_blob())
-                self.is_tracker_init = True
-            return
+			if init_regions is not None:
+				if init_regions:
+					self.tracker = cv.TrackerKCF_create()
+					self.tracker.init(frame, init_regions[0].to_blob())
+					self.is_tracker_init = True
+				return
 
-        success, blob = self.tracker.update(frame)
-        regions = [Region(*blob, self.colors[0], Shape.RECTANGLE)]
-        
-        if success:
-            return regions
-        return  # Turn error context var to true
+			success, blob = self.tracker.update(frame)
+			regions = [Region(*blob, self.colors[0], Shape.RECTANGLE)]
+			
+			if success:
+				return regions
+			return  # Turn error context var to true
 
     def dispatch(self, frame: np.ndarray, processed_frame_preview: bool) -> None:
-        self.tracked_frames += 1
-        regions: List[Region] = None
-        
-        if self.tracked_frames >= TRACKING_FRAMES_NUM or not self.is_tracker_init:
-            regions = self.detect(frame, False)
-            self.track(frame, regions)  # If the detection fails, the tracker is not reinitialized :)
-            self.tracked_frames = 0
-        else:
-            regions = self.track(frame)
-        
-        self.display.show(frame, regions, 'Tracked regions', .4)
+			self.tracked_frames += 1
+			regions: List[Region] = None
+			
+			if self.tracked_frames >= TRACKING_FRAMES_NUM or not self.is_tracker_init:
+				regions = self.detect(frame, False)
+				self.track(frame, regions)  # If the detection fails, the tracker is not reinitialized :)
+				self.tracked_frames = 0
+			else:
+				regions = self.track(frame)
+			
+			self.display.show(frame, regions, 'Tracked regions', .4)
 
 
     def loop(self, processed_frame_preview: bool) -> None:  # Blocking method
-        """
-        Start video capture and frames classification. Be aware that it's a blocking method (it enters a loop)
-        :param bool processed_frame_preview: am I supposed to show the processed frame?
-        """
-        if self.image:
-            img: np.ndarray = cv.imread(self.image)
-            self.detect_and_display(img, processed_frame_preview)
-            if cv.waitKey(0) == 27:  # Key ==> 'ESC'
-                return
+			"""
+			Start video capture and frames classification. Be aware that it's a blocking method (it enters a loop)
+			:param bool processed_frame_preview: am I supposed to show the processed frame?
+			"""
+			if self.image:
+				img: np.ndarray = cv.imread(self.image)
+				self.detect_and_display(img, processed_frame_preview)
+				if cv.waitKey(0) == 27:  # Key ==> 'ESC'
+					return
 
-        cap = cv.VideoCapture(int(self.video_source) if str.isnumeric(self.video_source) else self.video_source)
-        frames_number: int = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
-        if frames_number > 0:  # frames_num < 0 when the video source is a camera
-            self.times = np.empty(frames_number, dtype='f', order='C')
-            cap.set(cv.CAP_PROP_BUFFERSIZE, FRAME_BUFFER_SIZE)
-        if not cap.isOpened():
-            logging.error("Camera video stream can't be opened")
-            exit(1)
-        while True:
-            ret, frame = cap.read()
-            if frame is None:
-                break
-            self.dispatch(frame, processed_frame_preview)
-            # self.detect_and_display(frame, processed_frame_preview)
-            if cv.waitKey(1) == 27:  # Key ==> 'ESC'
-                break
-        # When classification is done, print the average time needed to classify each frame
-        if frames_number > 0:
-            logging.info(f"Average time needed to classify each frame {np.average(self.times[:self.times_index])}")
-            logging.info(f"Max time needed to classify each frame {np.amax(self.times[:self.times_index])}")
-            logging.info(f"Min time needed to classify each frame {np.amin(self.times[:self.times_index])}")
+			cap = cv.VideoCapture(int(self.video_source) if str.isnumeric(self.video_source) else self.video_source)
+			frames_number: int = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+			if frames_number > 0:  # frames_num < 0 when the video source is a camera
+				self.times = np.empty(frames_number, dtype='f', order='C')
+				cap.set(cv.CAP_PROP_BUFFERSIZE, FRAME_BUFFER_SIZE)
+			if not cap.isOpened():
+				logging.error("Camera video stream can't be opened")
+				exit(1)
+			while True:
+				ret, frame = cap.read()
+				if frame is None:
+					break
+				self.dispatch(frame, processed_frame_preview)
+				# self.detect_and_display(frame, processed_frame_preview)
+				if cv.waitKey(1) == 27:  # Key ==> 'ESC'
+					break
+			# When classification is done, print the average time needed to classify each frame
+			if frames_number > 0:
+				logging.info(f"Average time needed to classify each frame {np.average(self.times[:self.times_index])}")
+				logging.info(f"Max time needed to classify each frame {np.amax(self.times[:self.times_index])}")
+				logging.info(f"Min time needed to classify each frame {np.amin(self.times[:self.times_index])}")
